0c62ae33400672d57b6e19fec911cebdabf63154
Marcelle <marcelleb@bbd.co.za>
2021-02-14 15:19:00 +0200

    register endpoint added

---
+++ Controllers/RegisterController.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Night_Shadow.Models;
+using Okta.Sdk;
+using Okta.Sdk.Configuration;
+
+
+
+
+namespace Night_Shadow.Controllers
+{
+
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NameController : ControllerBase
+    {
+        [HttpPost]
+        public async Task PostAsync([FromBody] Register reg)
+        {
+            var oktaClient = new OktaClient();
+            var user = await oktaClient.Users.CreateUserAsync(new CreateUserWithPasswordOptions
+            {
+                Profile = new UserProfile
+                {
+                    FirstName = reg.FirstName,
+                    LastName = reg.LastName,
+                    Email = reg.Email,
+                    Login = reg.Email
+                },
+                Password = reg.Password,
+                Activate = true
+            });
+
+        }
+    }
+}
